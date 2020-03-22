c8f9849fbb0c5307dba19d9f096a2ba96eb731db
Mel Hiam <53319831+meledp@users.noreply.github.com>
2020-03-22 15:01:33 -0400

    Add files via upload

---
+++ games_table.sql
@@ -0,0 +1,12 @@
+INSERT INTO games (game_name)
+VALUES ('Persona');
+INSERT INTO games (game_name)
+VALUES ('Kingdom Hearts');
+INSERT INTO games (game_name)
+VALUES ('The Sims');
+INSERT INTO games (game_name)
+VALUES ('Overcooked');
+INSERT INTO games (game_name)
+VALUES ('Nier: Automata');
+INSERT INTO games (game_name)
+VALUES ('Stardew Valley');
