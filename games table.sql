367d909f21641e19fb1f0a14099e706479da66f3
Mel Hiam <53319831+meledp@users.noreply.github.com>
2020-03-22 15:03:18 -0400

    Update games table.sql

--- games table.sql
+++ games table.sql
@@ -1,12 +1,12 @@
-INSERT INTO games (id, game_name, game_logo)
-VALUES (1, 'Persona', LOAD_FILE('D:\\CIS 1512 Software Engineering\\game images\\Persona_5_cover_art.jpg'));
-INSERT INTO games (id, game_name, game_logo)
-VALUES (2, 'Nier: Automata', LOAD_FILE('D:\\CIS 1512 Software Engineering\\game images\\NierAutomata.jpg'));
-INSERT INTO games (id, game_name, game_logo)
-VALUES (3, 'Kingdom Hearts', LOAD_FILE('D:\\CIS 1512 Software Engineering\\game images\\KingdomHearts.jpg'));
-INSERT INTO games (id, game_name, game_logo)
-VALUES (4, 'The Sims 4', LOAD_FILE('D:\\CIS 1512 Software Engineering\\game images\\The-Sims-4.jpg'));
-INSERT INTO games (id, game_name, game_logo)
-VALUES (5, 'Stardew Valley', LOAD_FILE('D:\\CIS 1512 Software Engineering\\game images\\StardewValley.jpg'));
-INSERT INTO games (id, game_name, game_logo)
-VALUES (6, 'Overcooked', LOAD_FILE('D:\\CIS 1512 Software Engineering\\game images\\Overcooked.jpg'));
+INSERT INTO games (game_name, game_logo)
+VALUES ('Persona', LOAD_FILE('D:\\CIS 1512 Software Engineering\\game images\\Persona_5_cover_art.jpg'));
+INSERT INTO games (game_name, game_logo)
+VALUES ('Nier: Automata', LOAD_FILE('D:\\CIS 1512 Software Engineering\\game images\\NierAutomata.jpg'));
+INSERT INTO games (game_name, game_logo)
+VALUES ('Kingdom Hearts', LOAD_FILE('D:\\CIS 1512 Software Engineering\\game images\\KingdomHearts.jpg'));
+INSERT INTO games (game_name, game_logo)
+VALUES ('The Sims 4', LOAD_FILE('D:\\CIS 1512 Software Engineering\\game images\\The-Sims-4.jpg'));
+INSERT INTO games (game_name, game_logo)
+VALUES ('Stardew Valley', LOAD_FILE('D:\\CIS 1512 Software Engineering\\game images\\StardewValley.jpg'));
+INSERT INTO games (game_name, game_logo)
+VALUES ('Overcooked', LOAD_FILE('D:\\CIS 1512 Software Engineering\\game images\\Overcooked.jpg'));
