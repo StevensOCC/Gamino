ad11edc24e2fbf334aad290a04a9eccd18fa3d43
Mel Hiam <53319831+meledp@users.noreply.github.com>
2020-04-06 18:38:59 -0400

    Update games_table.sql

--- games_table.sql
+++ games_table.sql
@@ -1,12 +1,8 @@
 INSERT INTO games (game_name)
-VALUES ('Persona');
-INSERT INTO games (game_name)
-VALUES ('Kingdom Hearts');
-INSERT INTO games (game_name)
-VALUES ('The Sims');
-INSERT INTO games (game_name)
-VALUES ('Overcooked');
-INSERT INTO games (game_name)
-VALUES ('Nier: Automata');
-INSERT INTO games (game_name)
-VALUES ('Stardew Valley');
+VALUES 
+  ('Persona'),
+  ('Kingdom Hearts'),
+  ('The Sims'),
+  ('Overcooked'),
+  ('Nier: Automata'),
+  ('Stardew Valley');
