3eabf5b30d817408bb64c0384cee88e9c231e44b
Mel Hiam <53319831+meledp@users.noreply.github.com>
2020-04-07 10:42:42 -0400

    Delete conversation_input_table.sql

--- conversation_input_table.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO conversation_input(input_time, message)
-VALUES('9999-12-31 23:59:59', 'testing testing testing');
-INSERT INTO conversation_input(input_user_id)
-SELECT user_id
-FROM users;
